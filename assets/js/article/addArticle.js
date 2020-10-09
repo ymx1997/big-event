3eafd30e246f7402d1a0fe7f216c183c6f991a73
ymx1997 <1169212079@qq.com>
2020-10-09 14:47:49 +0800

    发布文章功能完成

--- assets/js/article/addArticle.js
+++ assets/js/article/addArticle.js
@@ -6,7 +6,7 @@ $.ajax({
     url: '/my/article/cates',
     success: function (res) {
         let str = template('tpl-category', res);
-        $('select[name=category]').html(str);
+        $('select[name=cate_id]').html(str);
         // 模板引擎处理完后，重新渲染select
         form.render('select');
     }
@@ -28,12 +28,12 @@ var options = {
 theImg.cropper(options);
 
 // 点击'选择封面'可选择图片
-$('button:contains("选择封面")').click(function(){
-    $('#file').click();
+$('button:contains("选择封面")').on('click', function () {
+    $('#file').trigger('click');
 })
 
 // 图片切换时，更换剪裁区图片
-$('#file').change(function(){
+$('#file').on('change', function () {
     // 找到文件对象
     var fileObj = this.files[0];
 
@@ -42,4 +42,46 @@ $('#file').change(function(){
 
     // 更换图片
     theImg.cropper('destroy').attr('src', url).cropper(options);
+})
+
+// ------------------- 完成添加功能 -----------------------
+// 利用事件委托机制，给form表单绑定提交事件
+$('form').on('submit', function (e) {
+    // 阻止默认行为
+    e.preventDefault();
+    // 收集表单数据 FormDate格式
+    let fd = new FormData(this);
+    // 替换FormData对象里面的一项
+    fd.set('content', tinyMCE.activeEditor.getContent())
+    // 剪裁图片
+    let canvas = theImg.cropper('getCroppedCanvas', {
+        width: 400,
+        height: 200
+    });
+    // 把图片转成blob形式
+    canvas.toBlob(function (blob) {
+        // 形参blbo就是转换后的结果
+        // 把文件追加到fd中
+        fd.append('cover_img', blob)
+        // 遍历fd对象，检查fd对象中是否包括了接口要求的5个参数
+        // fd.forEach((value, key) => {
+        //     console.log(key, value)
+        // })
+        // ----------- ajax提交接口，完成添加 ------------
+        $.ajax({
+            type: 'POST',
+            url: '/my/article/add',
+            data: fd,
+            // 提交formdata数据，必须加下面两个选项
+            processData: false,
+            contentType: false,
+            success: function (res) {
+                // console.log(res);
+                layer.msg(res.message);
+                if (res.status === 0) {
+                    location.href = '/article/articleList.html';
+                }
+            }
+        });
+    })
 })
